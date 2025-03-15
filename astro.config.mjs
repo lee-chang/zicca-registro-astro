c44b2c4bd14f3a3ce345ea151e7d5fc65217340b
Lee <ca.y25@hotmail.com>
2025-03-15 14:19:31 -0500

    Agregar soporte para Cloudflare en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: cloudflare(),
 });
